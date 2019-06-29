10ff2be56ff5eb8e5bd0513177ee15e2f69cfd7c
Tim Goll <tim@timgoll.de>
2019-06-29 23:29:08 +0200

    implemented the very basics

---
+++ gamemodes/terrortown/entities/weapons/weapon_ttt2_holydeagle.lua
@@ -0,0 +1,139 @@
+SWEP.Base = "weapon_tttbase"
+
+SWEP.Spawnable = true
+SWEP.AutoSpawnable = false
+SWEP.AdminSpawnable = true
+
+SWEP.HoldType = "pistol"
+
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+
+if SERVER then
+	AddCSLuaFile()
+
+	resource.AddFile("materials/vgui/ttt/icon_sidekickdeagle.vmt")
+
+	util.AddNetworkString("tttSidekickMSG")
+else
+	hook.Add("Initialize", "TTTInitSikiDeagleLang", function()
+		LANG.AddToLanguage("English", "ttt2_weapon_holydeagle_desc", "Shoot a player to make him holy, but be careful to shoot only innocent people.")
+		LANG.AddToLanguage("Deutsch", "ttt2_weapon_holydeagle_desc", "Schieße auf einen Spieler, um ihn heilig zu machen. Aber pass auf nur auf Unschuldige zu schießen.")
+	end)
+
+	SWEP.PrintName = "Holy Deagle"
+	SWEP.Author = "Mineotopia"
+
+	SWEP.Slot = 7
+
+	SWEP.ViewModelFOV = 54
+	SWEP.ViewModelFlip = false
+
+	SWEP.Category = "Deagle"
+	SWEP.Icon = "vgui/ttt/icon_sidekickdeagle.vtf"
+	SWEP.EquipMenuData = {
+		type = "Weapon",
+		desc = "ttt2_weapon_sidekickdeagle_desc"
+	}
+end
+
+-- dmg
+SWEP.Primary.Delay = 1
+SWEP.Primary.Recoil = 6
+SWEP.Primary.Automatic = false
+SWEP.Primary.NumShots = 1
+SWEP.Primary.Damage = 1
+SWEP.Primary.Cone = 0.00001
+SWEP.Primary.Ammo = ""
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.ClipMax = 1
+SWEP.Primary.DefaultClip = 1
+
+-- some other stuff
+SWEP.InLoadoutFor = nil
+SWEP.AllowDrop = true
+SWEP.IsSilent = false
+SWEP.NoSights = false
+SWEP.UseHands = true
+SWEP.Kind = WEAPON_EQUIP2
+SWEP.CanBuy = {}
+SWEP.LimitedStock = true
+SWEP.globalLimited = true
+SWEP.NoRandom = true
+
+-- view / world
+SWEP.ViewModel = "models/weapons/cstrike/c_pist_deagle.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_deagle.mdl"
+SWEP.Weight = 5
+SWEP.Primary.Sound = Sound("Weapon_Deagle.Single")
+
+SWEP.IronSightsPos = Vector(-6.361, -3.701, 2.15)
+SWEP.IronSightsAng = Vector(0, 0, 0)
+
+SWEP.notBuyable = true
+
+function SWEP:OnDrop()
+	self:Remove()
+end
+
+function ShootSidekick(target, dmginfo)
+	--local attacker = dmginfo:GetAttacker()
+
+	--if not attacker:IsPlayer() or not target:IsPlayer() or not IsValid(attacker:GetActiveWeapon())
+	--	or not attacker:IsTerror() or not IsValid(target) or not target:IsTerror() then return end
+
+	--if target:GetSubRole() == ROLE_JACKAL or target:GetSubRole() == ROLE_SIDEKICK then
+	--	attacker:PrintMessage(HUD_PRINTTALK, "You can't shoot a Jackal/Sidekick as Sidekick!")
+	--	return
+	--end
+
+	--AddSidekick(target, attacker)
+
+	--net.Start("tttSidekickMSG")
+
+	--net.WriteEntity(target)
+
+	--net.Send(attacker)
+
+end
+
+
+if SERVER then
+	hook.Add("ScalePlayerDamage", "HolyHitReg", function(ply, hitgroup, dmginfo)
+		local attacker = dmginfo:GetAttacker()
+		if GetRoundState() ~= ROUND_ACTIVE or not attacker or not IsValid(attacker)
+			or not attacker:IsPlayer() or not IsValid(attacker:GetActiveWeapon()) then return end
+
+		local weap = attacker:GetActiveWeapon()
+
+		if weap:GetClass() ~= "weapon_ttt2_holydeagle" then return end
+
+		--ShootSidekick(ply, dmginfo)
+		dmginfo:SetDamage(0)
+		return true
+	end)
+end
+
+
+-- auto add sidekick weapon into jackal shop
+-- hook.Add("LoadedFallbackShops", "SidekickDeagleAddToShop", function()
+-- 	if JACKAL and SIDEKICK and JACKAL.fallbackTable then
+-- 		AddWeaponIntoFallbackTable("weapon_ttt2_sidekickdeagle", JACKAL)
+-- 	end
+-- end)
+
+-- if CLIENT then
+-- 	hook.Add("TTT2FinishedLoading", "InitSikiMsgText", function()
+-- 		LANG.AddToLanguage("English", "ttt2_siki_shot", "Successfully shot {name} as Sidekick!")
+-- 		LANG.AddToLanguage("Deutsch", "ttt2_siki_shot", "Erfolgreich {name} als Sidekick geschossen!")
+-- 	end)
+
+-- 	net.Receive("tttSidekickMSG", function(len)
+-- 		local target = net.ReadEntity()
+
+-- 		if not target or not IsValid(target) then return end
+
+-- 		local text = LANG.GetParamTranslation("ttt2_siki_shot", {name = target:GetName()})
+-- 		MSTACK:AddMessage(text)
+-- 	end)
+-- end
---
+++ lua/terrortown/entities/roles/priest/shared.lua
@@ -0,0 +1,90 @@
+if SERVER then
+	AddCSLuaFile()
+
+	resource.AddFile('materials/vgui/ttt/dynamic/roles/icon_pri.vmt')
+end
+
+ROLE.Base = 'ttt_role_base'
+
+ROLE.color = Color(185, 210, 95, 255)
+ROLE.dkcolor = Color(150, 175, 65, 255)
+ROLE.bgcolor = Color(215, 240, 130, 255)
+ROLE.abbr = "pri"
+ROLE.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
+ROLE.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
+ROLE.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
+ROLE.preventFindCredits = true
+ROLE.preventKillCredits = true
+ROLE.preventTraitorAloneCredits = true
+ROLE.preventWin = false
+
+roles.InitCustomTeam(ROLE.name, {
+    icon = 'vgui/ttt/dynamic/roles/icon_pri',
+    color = ROLE.color
+})
+ROLE.defaultTeam = TEAM_INNOCENT
+
+ROLE.conVarData = {
+	pct = 0.15, -- necessary: percentage of getting this role selected (per player)
+	maximum = 1, -- maximum amount of roles in a round
+	minPlayers = 7, -- minimum amount of players until this role is able to get selected
+	credits = 0, -- the starting credits of a specific role
+	shopFallback = SHOP_DISABLED,
+	togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+	random = 33
+}
+
+hook.Add('TTT2FinishedLoading', 'PriestInitT', function()
+
+	if CLIENT then
+		LANG.AddToLanguage('English', PRIEST.name, 'Priest')
+		LANG.AddToLanguage('English', TEAM_INNOCENT, 'TEAM Priest')
+		LANG.AddToLanguage('English', 'info_popup_' .. PRIEST.name,
+			[[You are the Priest!
+			Use your holy deagle to convert players into your brotherhood. But be careful, only innocent players behave well.]])
+		LANG.AddToLanguage('English', 'body_found_' .. PRIEST.abbr, 'They were a Priest.')
+        LANG.AddToLanguage('English', 'search_role_' .. PRIEST.abbr, 'This person was a Priest!')
+		LANG.AddToLanguage('English', 'target_' .. PRIEST.name, 'Priest')
+		LANG.AddToLanguage('English', 'ttt2_desc_' .. PRIEST.name, [[The Priest needs to win with the innocents!]])
+		LANG.AddToLanguage('English', 'hilite_win_' .. TEAM_INNOCENT, 'THE PRIEST WON') -- name of base role of a team
+		LANG.AddToLanguage('English', 'win_' .. TEAM_INNOCENT, 'The Priest has won!') -- teamname
+		LANG.AddToLanguage('English', 'ev_win_' .. TEAM_INNOCENT, 'The evil Priest won the round!')
+		LANG.AddToLanguage('English', 'credit_' .. PRIEST.abbr .. '_all', 'Priests, you have been awarded {num} equipment credit(s) for your performance.')
+
+		---------------------------------
+
+		-- maybe this language as well...
+		LANG.AddToLanguage('Deutsch', PRIEST.name, 'Priester')
+		LANG.AddToLanguage('Deutsch', TEAM_INNOCENT, 'TEAM Priest')
+		LANG.AddToLanguage('Deutsch', 'info_popup_' .. PRIEST.name,
+			[[Du bist ein Priester!
+			Benutze deine Heilige Deagle um andere Spieler in deine Bruderschaft aufzunehmen. Aber sei vorsichtig, nur Unschuldige verhalten sich gut.]])
+		LANG.AddToLanguage('Deutsch', 'body_found_' .. PRIEST.abbr, 'Er war ein Priester!')
+		LANG.AddToLanguage('Deutsch', 'search_role_' .. PRIEST.abbr, 'Diese Person war ein Priester!')
+		LANG.AddToLanguage('Deutsch', 'target_' .. PRIEST.name, 'Priester')
+		LANG.AddToLanguage('Deutsch', 'ttt2_desc_' .. PRIEST.name, [[Der Priester gewinnt zusammen mit den Unschuldigen!]])
+		LANG.AddToLanguage('Deutsch', 'hilite_win_' .. TEAM_INNOCENT, 'THE PRIEST WON') -- name of base role of a team
+		LANG.AddToLanguage('Deutsch', 'win_' .. TEAM_INNOCENT, 'Der Priester hat gewonnen!') -- teamname
+		LANG.AddToLanguage('Deutsch', 'ev_win_' .. TEAM_INNOCENT, 'Der böse Priester hat die Runde gewonnen!')
+		LANG.AddToLanguage('Deutsch', 'credit_' .. PRIEST.abbr .. '_all', 'Priester, dir wurde(n) {num} Ausrüstungs-Credit(s) für deine Leistung gegeben.')
+	end
+end) 
+
+if SERVER then
+	local function InitRolePriest(ply)
+		ply:GiveEquipmentWeapon('weapon_ttt2_holydeagle')
+    end
+ 
+    hook.Add('TTT2UpdateSubrole', 'TTT2PriestGiveHolyDeagle_UpdateSubtole', function(ply, old, new) -- called on normal role set
+        if new == ROLE_PRIEST then
+            InitRolePriest(ply)
+        elseif old == ROLE_PRIEST then
+            ply:StripWeapon('weapon_ttt2_holydeagle')
+        end
+    end)
+   
+    hook.Add('PlayerSpawn', 'TTT2PriestGiveHolyDeagle_PlayerSpawn', function(ply) -- called on player respawn
+        if ply:GetSubRole() ~= ROLE_PRIEST then return end
+        InitRolePriest(ply)
+    end)
+end
