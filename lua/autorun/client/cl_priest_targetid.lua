ecd047c27a833f4b431b383c53228ae8e913a093
Tim Goll <tim@timgoll.de>
2020-02-10 18:55:56 +0100

    switch to reworked targetID system

--- lua/autorun/client/cl_priest_targetid.lua
+++ lua/autorun/client/cl_priest_targetid.lua
@@ -1,23 +1,25 @@
 local material_in_brotherhood = Material('vgui/ttt/dynamic/roles/is_in_brotherhood')
 
 -- handle looking at sodas
-hook.Add('TTTRenderEntityInfo', 'ttt2_priest_highlight_players', function(data, params)
+hook.Add('TTTRenderEntityInfo', 'ttt2_priest_highlight_players', function(tData)
 	-- while client is setting up, ignore missing PRIEST
 	if not PRIEST then return end
 
+	local ent = tData:GetEntity()
+
 	-- has to be a player
-	if not data.ent:IsPlayer() then return end
+	if not ent:IsPlayer() then return end
 
 	-- only add text to brotherhood players
-	if not PRIEST_DATA:IsBrother(data.ent) or not PRIEST_DATA:IsBrother(LocalPlayer()) then return end
+	if not PRIEST_DATA:IsBrother(ent) or not PRIEST_DATA:IsBrother(LocalPlayer()) then return end
 
-	params.displayInfo.desc[#params.displayInfo.desc + 1] = {
-		text = LANG.GetTranslation('ttt2_priest_player_brother'),
-		color = PRIEST.dkcolor
-	}
+	tData:AddDescriptionLine(
+		LANG.GetTranslation('ttt2_priest_player_brother'),
+		PRIEST.dkcolor
+	)
 
-	params.displayInfo.icon[#params.displayInfo.icon + 1] = {
-		material = material_in_brotherhood,
-		color = PRIEST.dkcolor
-	}
+	tData:AddIcon(
+		material_in_brotherhood,
+		PRIEST.dkcolor
+	)
 end)
