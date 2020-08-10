04cc05fdb23b113a0791474341dfb45d8a57784b
Tim Goll <tim@timgoll.de>
2020-08-10 10:58:55 +0200

    switched to new folder structure

---
+++ lua/terrortown/lang/italiano/priest.lua
@@ -0,0 +1,26 @@
+local L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[PRIEST.name] = "Prete"
+L["info_popup_" .. PRIEST.name] = [[Sei il Prete!
+Usa la tua Deagle Benedetta per convertire i giocatori alla tua confraternita. Ma fai attenzione, solo i giocatori Innocenti si comporteranno bene.]]
+L["body_found_" .. PRIEST.abbr] = "Era un Prete."
+L["search_role_" .. PRIEST.abbr] = "Questa persona era un Prete!"
+L["target_" .. PRIEST.name] = "Prete"
+L["ttt2_desc_" .. PRIEST.name] = [[Il Prete deve vincere con gli innocenti!]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_priest_was_priest"] = "Questo giocatore sembra essere un membro della confraternita."
+--L["ttt2_priest_added"] = "It seems like a player was added to the brotherhood."
+--L["ttt2_priest_brother_died"] = "It seems like a brother died."
+--L["ttt2_priest_detective"] = "The holy spirit was used to hurt a detective."
+--L["ttt2_priest_infected"] = "The holy spirit was used to kill an infected."
+--L["ttt2_priest_necromancer"] = "The holy spirit was used to kill a necromancer."
+--L["ttt2_priest_sidekick"] = "The holy spirit was used to kill a sidekick."
+--L["ttt2_priest_died"] = "The holy spirit killed a priest."
+--L["ttt2_priest_priest"] = "You can't add a priest to the brotherhood."
+--L["ttt2_priest_marker"] = "It seems like the priest shot a bucket full of color."
+--L["ttt2_priest_brother_jackal"] = "Shooting the priest converted the whole brotherhood to sidekicks."
+--L["ttt2_priest_brother_necromancer"] = "Reviving the priest as a zombie converted the whole brotherhood to zombies."
+--L["ttt2_priest_brother_infected"] = "Killing the priest converted the whole brotherhood to infected."
+--L["ttt2_priest_player_brother"] = "PLAYER IS IN BROTHERHOOD"
